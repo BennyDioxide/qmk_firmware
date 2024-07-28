b09ebabf99648b9895a417586f8dc90abf25eb05
BennyDioxide <bennystyang@proton.me>
2024-07-28 17:50:32 +0800

    feat: Add extend layer

--- keyboards/nuphy/air75_v2/ansi/keymaps/via-ext/keymap.c
+++ keyboards/nuphy/air75_v2/ansi/keymaps/via-ext/keymap.c
@@ -17,6 +17,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include QMK_KEYBOARD_H
 
+#define KC_EXT (LT(6, KC_CAPS))
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 // layer Mac
@@ -42,18 +44,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	KC_ESC, 	KC_F1,  	KC_F2,  	KC_F3, 		KC_F4,  	KC_F5,  	KC_F6,  	KC_F7,  	KC_F8,  	KC_F9, 		KC_F10, 	KC_F11, 	KC_F12, 	KC_PSCR,	KC_INS,		KC_DEL,
 	KC_GRV, 	KC_1,   	KC_2,   	KC_3,  		KC_4,   	KC_5,   	KC_6,   	KC_7,   	KC_8,   	KC_9,  		KC_0,   	KC_MINS,	KC_EQL, 				KC_BSPC,	KC_PGUP,
 	KC_TAB, 	KC_Q,   	KC_W,   	KC_E,  		KC_R,   	KC_T,   	KC_Y,   	KC_U,   	KC_I,   	KC_O,  		KC_P,   	KC_LBRC,	KC_RBRC, 				KC_BSLS,	KC_PGDN,
-	KC_CAPS,	KC_A,   	KC_S,   	KC_D,  		KC_F,   	KC_G,   	KC_H,   	KC_J,   	KC_K,   	KC_L,  		KC_SCLN,	KC_QUOT, 	 						KC_ENT,		KC_HOME,
+	KC_EXT, 	KC_A,   	KC_S,   	KC_D,  		KC_F,   	KC_G,   	KC_H,   	KC_J,   	KC_K,   	KC_L,  		KC_SCLN,	KC_QUOT, 	 						KC_ENT,		KC_HOME,
 	KC_LSFT,				KC_Z,   	KC_X,   	KC_C,  		KC_V,   	KC_B,   	KC_N,   	KC_M,   	KC_COMM,	KC_DOT,		KC_SLSH,				KC_RSFT,	KC_UP,		KC_END,
 	KC_LCTL,	KC_LGUI,	KC_LALT,										KC_SPC, 							KC_RALT,	MO(4),   	KC_RCTL,				KC_LEFT,	KC_DOWN,    KC_RGHT),
 
 // layer win Colemak-DH
 // Rewrite some function keys to prevent fallbacking to the mac layer
 [3] = LAYOUT_ansi_84(
-    _______, 	KC_F1,  	KC_F2,  	KC_F3, 		KC_F4,  	KC_F5,  	KC_F6,  	KC_F7,  	KC_F8,  	KC_F9, 		KC_F10, 	KC_F11, 	KC_F12, 	KC_PSCR,	KC_INS,		KC_DEL,
-    _______, 	_______, 	_______, 	_______, 	_______, 	_______, 	_______, 	_______, 	_______, 	_______, 	_______, 	_______, 	_______, 				_______, 	_______,
-    _______, 	_______, 	_______, 	KC_F,    	KC_P,    	KC_B,    	KC_J,    	KC_L,    	KC_U,    	KC_Y,    	KC_SCLN, 	_______, 	_______, 				_______, 	_______,
-    _______, 	_______, 	KC_R,    	KC_S,    	KC_T,    	_______, 	KC_M,    	KC_N,    	KC_E,    	KC_I,    	KC_O,    	_______, 							_______, 	_______,
-    _______, 				KC_X,    	KC_C,    	KC_D,    	KC_V,    	KC_Z,    	KC_K,    	KC_H,    	_______, 	_______, 	_______, 				_______, 	_______, 	_______,
+    _______,	KC_F1,  	KC_F2,  	KC_F3, 		KC_F4,  	KC_F5,  	KC_F6,  	KC_F7,  	KC_F8,  	KC_F9, 		KC_F10, 	KC_F11, 	KC_F12, 	KC_PSCR,	KC_INS,		KC_DEL,
+    _______,	_______, 	_______, 	_______, 	_______, 	_______, 	_______, 	_______, 	_______, 	_______, 	_______, 	_______, 	_______, 				_______, 	_______,
+    _______,	_______, 	_______, 	KC_F,    	KC_P,    	KC_B,    	KC_J,    	KC_L,    	KC_U,    	KC_Y,    	KC_SCLN, 	_______, 	_______, 				_______, 	_______,
+    KC_EXT, 	_______, 	KC_R,    	KC_S,    	KC_T,    	_______, 	KC_M,    	KC_N,    	KC_E,    	KC_I,    	KC_O,    	_______, 							_______, 	_______,
+    _______,				KC_X,    	KC_C,    	KC_D,    	KC_V,    	KC_Z,    	KC_K,    	KC_H,    	_______, 	_______, 	_______, 				_______, 	_______, 	_______,
     KC_LCTL,	KC_LGUI,	KC_LALT, 										_______, 							KC_RALT,	MO(4),   	KC_RCTL, 				_______, 	_______, 	_______),
 
 
@@ -73,5 +75,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	_______, 	_______,   	_______,   	_______,  	_______,   	_______,   	_______,   	_______,   	_______,   	_______,  	_______,   	_______,	_______, 				_______,	_______,
 	_______,	_______,   	_______,   	_______,  	_______,   	_______,   	_______,   	_______,   	_______,   	_______,  	_______,	_______, 	 						_______,	_______,
 	_______,				_______,   	_______,   	_______,  	_______,   	_______,   	_______,   	_______,   	SIDE_SPD,	SIDE_SPI,	_______,				_______,	SIDE_VAI,	_______,
-	_______,	_______,	_______,										_______, 							_______,	MO(5),   	_______,				SIDE_MOD,	SIDE_VAD,   SIDE_HUI)
+	_______,	_______,	_______,										_______, 							_______,	MO(5),   	_______,				SIDE_MOD,	SIDE_VAD,   SIDE_HUI),
+
+// Extend layer
+[6] = LAYOUT_ansi_84(
+	KC_CAPS, 	KC_BRID,   	KC_BRIU,    _______,  	_______,   	_______,   	_______,   	KC_MPRV,	KC_MPLY,	KC_MNXT,	KC_MUTE,	KC_VOLD, 	KC_VOLU,	_______,	_______,	_______,
+	_______, 	KC_F1,  	KC_F2,  	KC_F3, 		KC_F4,  	KC_F5,  	KC_F6,  	KC_F7,  	KC_F8,  	KC_F9,  	KC_F10, 	KC_F11, 	KC_F12, 				_______,	_______,
+	_______, 	KC_ESC,   	KC_BTN4,	KC_FIND,	KC_BTN5,	_______,	KC_PGUP,	KC_HOME,	KC_UP,  	KC_END, 	KC_CAPS,	_______,	_______, 				_______,	_______,
+	_______,	KC_LALT,   	KC_LGUI,	KC_LSFT,	KC_LCTL,	KC_RALT,	KC_PGDN,	KC_LEFT,	KC_DOWN,	KC_RGHT,	KC_DEL, 	KC_APP, 	 						_______,	_______,
+	_______,				KC_CUT, 	KC_COPY,	_______,	KC_PSTE,	KC_UNDO,	_______,	KC_BSPC,	SIDE_SPD,	SIDE_SPI,	_______,				_______,	SIDE_VAI,	_______,
+	_______,	_______,	_______,										KC_ENT, 							_______,	MO(5),   	_______,				SIDE_MOD,	SIDE_VAD,   SIDE_HUI),
 };

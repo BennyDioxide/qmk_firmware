1e696623caa7d386a3842a5e93a3b46dc7ad9c38
BennyDioxide <bennystyang@proton.me>
2024-07-28 21:04:20 +0800

    fix: Undo still unusable

--- keyboards/nuphy/air75_v2/ansi/keymaps/via-ext/keymap.c
+++ keyboards/nuphy/air75_v2/ansi/keymaps/via-ext/keymap.c
@@ -83,7 +83,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	_______, 	KC_F1,  	KC_F2,  	KC_F3, 		KC_F4,  	KC_F5,  	KC_F6,  	KC_F7,  	KC_F8,  	KC_F9,  	KC_F10, 	KC_F11, 	KC_F12, 				_______,	_______,
 	_______, 	KC_ESC,   	KC_BTN4,	KC_FIND,	KC_BTN5,	_______,	KC_PGUP,	KC_HOME,	KC_UP,  	KC_END, 	KC_CAPS,	_______,	_______, 				_______,	_______,
 	MO(6),  	KC_LALT,   	KC_LGUI,	KC_LSFT,	KC_LCTL,	KC_RALT,	KC_PGDN,	KC_LEFT,	KC_DOWN,	KC_RGHT,	KC_DEL, 	KC_APP, 	 						_______,	_______,
-	_______,				KC_CUT, 	KC_COPY,	KC_LGUI,	KC_PSTE,	S(KC_Z),	_______,	KC_BSPC,	KC_TAB, 	SIDE_SPI,	_______,				_______,	SIDE_VAI,	_______,
+	_______,				KC_CUT, 	KC_COPY,	KC_LGUI,	KC_PSTE,	C(KC_Z),	_______,	KC_BSPC,	KC_TAB, 	SIDE_SPI,	_______,				_______,	SIDE_VAI,	_______,
 	_______,	_______,	_______,										KC_ENT, 							_______,	MO(5),   	_______,				SIDE_MOD,	SIDE_VAD,   SIDE_HUI),
 
 // Symbols layer
